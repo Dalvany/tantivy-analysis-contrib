8da38655d1a7c18a75ba8f9f719654828c8dd74e
Dalvany <9901407+Dalvany@users.noreply.github.com>
2022-08-07 17:54:18 +0200

    Test

--- src/phonetic/types.rs
+++ src/phonetic/types.rs
@@ -2,20 +2,20 @@
 ///
 /// If `None` is provided then the phonetic encoder will choose its default.
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct MaxCodeLength(pub(crate) Option<usize>);
+pub struct MaxCodeLength(pub Option<usize>);
 
 /// If text contains multiple words they all get encode if `true` otherwise
 /// only the first word will be encoded.
 ///
 /// If `None` is provided, it will be `true`.
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct Concat(pub(crate) Option<bool>);
+pub struct Concat(pub Option<bool>);
 
 /// Allow to set the maximum length in [BeiderMorse](PhoneticAlgorithm::BeiderMorse).
 ///
 /// If `None` it will use 20.
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct MaxPhonemeNumber(pub(crate) Option<usize>);
+pub struct MaxPhonemeNumber(pub Option<usize>);
 
 /// This is Daitch-Mokotoff rules. They will be parsed.
 /// You can find commons-codec's rules [here](https://github.com/apache/commons-codec/blob/rel/commons-codec-1.15/src/main/resources/org/apache/commons/codec/language/dmrules.txt)
@@ -23,7 +23,7 @@ pub struct MaxPhonemeNumber(pub(crate) Option<usize>);
 /// They can be provided using feature `embedded_dm`.
 #[cfg(not(feature = "embedded_dm"))]
 #[derive(Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct DMRule(pub(crate) String);
+pub struct DMRule(pub String);
 
 /// This is Daitch-Mokotoff rules. They will be parsed.
 /// You can find commons-codec's rules [here](https://github.com/apache/commons-codec/blob/rel/commons-codec-1.15/src/main/resources/org/apache/commons/codec/language/dmrules.txt)
@@ -31,37 +31,37 @@ pub struct DMRule(pub(crate) String);
 /// If `None` is provided then the embedded rules will be used.
 #[cfg(feature = "embedded_dm")]
 #[derive(Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct DMRule(pub(crate) Option<String>);
+pub struct DMRule(pub Option<String>);
 
 /// Boolean to apply folding (`true`) in Daitch-Mokotoff.
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct Folding(pub(crate) bool);
+pub struct Folding(pub bool);
 
 /// Boolean to allow (`true`) or disallow (`false`) branching
 /// for Daitch-Mokotoff.
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct Branching(pub(crate) bool);
+pub struct Branching(pub bool);
 
 /// This boolean allow to generate alternate code, in double metaphone,
 /// if different from primary.
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct Alternate(pub(crate) bool);
+pub struct Alternate(pub bool);
 
 /// This boolean indicates if Nysiis algorithm should be strict or not.
 ///
 /// Default to `true`.
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct Strict(pub(crate) Option<bool>);
+pub struct Strict(pub Option<bool>);
 
 /// This is the mapping for each latin letter for Soundex and Refined
 /// Soundex.
 ///
 /// The default is [DEFAULT_US_ENGLISH_MAPPING_SOUNDEX].
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct Mapping(pub(crate) Option<[char; 26]>);
+pub struct Mapping(pub Option<[char; 26]>);
 
 /// Indicate, for Soundex, if `H` and `W` should be treated as silence.
 ///
 /// Default to `true`.
 #[derive(Copy, Clone, Debug, Ord, PartialOrd, Eq, PartialEq, Hash)]
-pub struct SpecialHW(pub(crate) Option<bool>);
+pub struct SpecialHW(pub Option<bool>);
